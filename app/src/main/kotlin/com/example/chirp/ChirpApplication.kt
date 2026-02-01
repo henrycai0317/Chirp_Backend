ae5a06dc315c3955393544b0cc6c64eae9d474a9
Henry Cai <alaba.havai@gmail.com>
2026-02-01 18:54:22 +0800

    feat: add PasswordEncoder for secure password handling and include Spring Security dependency

--- app/src/main/kotlin/com/example/chirp/ChirpApplication.kt
+++ app/src/main/kotlin/com/example/chirp/ChirpApplication.kt
@@ -1,11 +1,7 @@
 package com.example.chirp
 
-import com.example.chirp.infra.database.entities.UserEntity
-import com.example.chirp.infra.database.repositories.UserRepository
-import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class ChirpApplication
---
+++ user/src/main/kotlin/com/example/chirp/infra/security/PasswordEncoder.kt
@@ -0,0 +1,14 @@
+package com.example.chirp.infra.security
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class PasswordEncoder {
+    private val bcrypt = BCryptPasswordEncoder()
+
+    fun encode(rawPassword: String): String? = bcrypt.encode(rawPassword)
+    fun matches(rawPassword: String, encodedPassword: String): Boolean {
+        return bcrypt.matches(rawPassword, encodedPassword)
+    }
+}
