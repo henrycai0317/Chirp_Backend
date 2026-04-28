814bfb13f7d5846ab93e872b6d7fda225d2dec46
Henry Cai <alaba.havai@gmail.com>
2026-04-28 14:33:08 +0800

    feat: add NotificationUserEventListener and configure RabbitMQ bindings for user events

---
+++ notification/src/main/kotlin/com/example/chirp/infra/message_queue/NotificationUserEventListener.kt
@@ -0,0 +1,30 @@
+package com.example.chirp.infra.message_queue
+
+import com.example.chirp.domain.events.user.UserEvent
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class NotificationUserEventListener {
+
+    @RabbitListener(queues = [MessageQueues.NOTIFICATION_USER_EVENTS])
+    @Transactional
+    fun handleUserEvent(event: UserEvent) {
+        when (event) {
+            is UserEvent.Created -> {
+                println("User created!")
+            }
+
+            is UserEvent.RequestResendVerification -> {
+                println("Request resend verification!")
+            }
+
+            is UserEvent.RequestResetPassword -> {
+                println("Request reset password!")
+            }
+
+            else -> Unit
+        }
+    }
+}
