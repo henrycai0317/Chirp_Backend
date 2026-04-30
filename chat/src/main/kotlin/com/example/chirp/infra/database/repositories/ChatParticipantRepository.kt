5852364361d95852ff85a5c5f90de58521b007e7
Henry Cai <alaba.havai@gmail.com>
2026-04-30 19:02:15 +0800

    feat: add ChatRepository for managing Chat entities and fetching by user ID

--- chat/src/main/kotlin/com/example/chirp/infra/database/repositories/ChatParticipantRepository.kt
+++ chat/src/main/kotlin/com/example/chirp/infra/database/repositories/ChatParticipantRepository.kt
@@ -5,13 +5,15 @@ import com.example.chirp.infra.database.entities.ChatParticipantEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
-interface ChatParticipantRepository: JpaRepository<ChatParticipantEntity, UserId> {
+interface ChatParticipantRepository : JpaRepository<ChatParticipantEntity, UserId> {
     fun findByUserIdIn(userIds: List<UserId>): Set<ChatParticipantEntity>
 
-    @Query("""
+    @Query(
+        """
         SELECT p
         FROM ChatParticipantEntity p
         WHERE LOWER(p.username) = :query OR LOWER(p.email) = :query
-    """)
+    """
+    )
     fun findByEmailOrUsername(query: String): ChatParticipantEntity?
 }
---
+++ chat/src/main/kotlin/com/example/chirp/infra/database/repositories/ChatRepository.kt
@@ -0,0 +1,36 @@
+package com.example.chirp.infra.database.repositories
+
+import com.example.chirp.domain.type.ChatId
+import com.example.chirp.domain.type.UserId
+import com.example.chirp.infra.database.entities.ChatEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface ChatRepository: JpaRepository<ChatEntity, ChatId> {
+    @Query("""
+        SELECT c
+        FROM ChatEntity c
+        LEFT JOIN FETCH c.participants
+        LEFT JOIN FETCH c.creator
+        WHERE c.id = :id
+        AND EXISTS (
+            SELECT 1
+            FROM c.participants p
+            WHERE p.userId = :userId
+        )
+    """)
+    fun findChatById(id: ChatId, userId: UserId): ChatEntity?
+
+    @Query("""
+        SELECT c
+        FROM ChatEntity c
+        LEFT JOIN FETCH c.participants
+        LEFT JOIN FETCH c.creator
+        WHERE EXISTS (
+            SELECT 1
+            FROM c.participants p
+            WHERE p.userId = :userId
+        )
+    """)
+    fun findAllByUserId(userId: UserId): List<ChatEntity>
+}
