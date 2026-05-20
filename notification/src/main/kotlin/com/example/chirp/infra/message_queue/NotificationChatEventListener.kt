d8e54c9e101ad7949c462663c9cef1992d2ac246
Henry Cai <alaba.havai@gmail.com>
2026-05-20 20:35:09 +0800

    feat: add chat event listener for push notifications and exception handler for invalid device tokens

---
+++ notification/src/main/kotlin/com/example/chirp/infra/message_queue/NotificationChatEventListener.kt
@@ -0,0 +1,31 @@
+package com.example.chirp.infra.message_queue
+
+import com.example.chirp.domain.events.chat.ChatEvent
+import com.example.chirp.domain.events.user.UserEvent
+import com.example.chirp.service.EmailService
+import com.example.chirp.service.PushNotificationService
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.Duration
+
+@Component
+class NotificationChatEventListener(
+    private val pushNotificationService: PushNotificationService
+) {
+
+    @RabbitListener(queues = [MessageQueues.NOTIFICATION_CHAT_EVENTS])
+    fun handleUserEvent(event: ChatEvent) {
+        when(event) {
+            is ChatEvent.NewMessage -> {
+                pushNotificationService.sendNewMessageNotifications(
+                    recipientUserIds = event.recipientIds.toList(),
+                    senderUserId = event.senderId,
+                    senderUsername = event.senderUsername,
+                    message = event.message,
+                    chatId = event.chatId
+                )
+            }
+        }
+    }
+}
